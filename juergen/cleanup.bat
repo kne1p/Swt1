9bef5064d1dc43918c51cb029e180b7c7f0e4559
Chris <c.juelg@gmail.com>
2011-05-01 02:27:33 +0200

    header ausgelagert, VCS teil ausgebaut, private constructor

---
+++ juergen/cleanup.bat
@@ -0,0 +1,8 @@
+rem sollte klappen...
+del *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig *synctex.gz
+cd static
+del *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+cd ..
+cd tuts
+del *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+cd ..
